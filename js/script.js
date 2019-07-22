4e85959e0eddb72759540d3334fb25f6fbff2bda
ait <ait@ex.com>
2019-07-22 20:59:24 +0900

    UI Update inputView

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-'script.js'
+// 'script.js'
 var main = document.querySelector(".main");
 var sidebar = document.querySelector(".sidebar");
 var menutoggle = document.querySelector("#menu-toggle");
@@ -17,29 +17,49 @@ menutoggle.addEventListener("click", function(e) {
   }
 });
 
+
+/*-------------------------*/
+
+function result() {
+  let age = document.querySelector("input.age");
+  console.log("hoge");
+
+  var min = 20;
+  var max = 48;
+  var v = Math.floor(Math.random() * (max + 1 - min)) + min;
+  age.value = v;
+  return false;
+}
+
+function ex() {
+  console.log("日付が変更されました");
+  result();
+}
+
+
 /*
   Sort
   https://mdbootstrap.com/docs/jquery/tables/search/
   https://datatables.net/examples/advanced_init/length_menu.html
 */
 
-$(document).ready(function() {
-  $('#example').DataTable({
-    // "order": [[ 1, "desc" ]]
-    "lengthMenu": [
-      [5, 10, 15, -1],
-      [5, 10, 15, "All"]
-    ]
-  });
-});
+// $(document).ready(function() {
+//   $('#example').DataTable({
+//     // "order": [[ 1, "desc" ]]
+//     "lengthMenu": [
+//       [5, 10, 15, -1],
+//       [5, 10, 15, "All"]
+//     ]
+//   });
+// });
 
 
-$('button.adaptation').on('click', function(e) {
-  $elem = $(e.target);
-
-  if ($elem.hasClass('active')) {
-    $elem.removeClass('active');
-  } else {
-    $elem.addClass('active');
-  }
-});
+// $('button.adaptation').on('click', function(e) {
+//   $elem = $(e.target);
+//
+//   if ($elem.hasClass('active')) {
+//     $elem.removeClass('active');
+//   } else {
+//     $elem.addClass('active');
+//   }
+// });
