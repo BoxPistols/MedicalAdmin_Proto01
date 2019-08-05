563a73451ece94318b19fd07aa6781b884806abb
ait <ait@ex.com>
2019-08-05 21:50:09 +0900

    ad open filter

--- js/script.js
+++ js/script.js
@@ -43,23 +43,23 @@ function ex() {
   https://datatables.net/examples/advanced_init/length_menu.html
 */
 
-// $(document).ready(function() {
-//   $('#example').DataTable({
-//     // "order": [[ 1, "desc" ]]
-//     "lengthMenu": [
-//       [5, 10, 15, -1],
-//       [5, 10, 15, "All"]
-//     ]
-//   });
-// });
+$(document).ready(function() {
+  $('#example').DataTable({
+    // "order": [[ 1, "desc" ]]
+    "lengthMenu": [
+      [5, 10, 15, -1],
+      [5, 10, 15, "All"]
+    ]
+  });
+});
 
 
-// $('button.adaptation').on('click', function(e) {
-//   $elem = $(e.target);
-//
-//   if ($elem.hasClass('active')) {
-//     $elem.removeClass('active');
-//   } else {
-//     $elem.addClass('active');
-//   }
-// });
+$('button.adaptation').on('click', function(e) {
+  $elem = $(e.target);
+
+  if ($elem.hasClass('active')) {
+    $elem.removeClass('active');
+  } else {
+    $elem.addClass('active');
+  }
+});
